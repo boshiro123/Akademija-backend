07aa0e662b991c0f5620a3041fd7f98a7109c337
boshiro123 <b.shiroki@mail.ru>
2025-03-06 17:51:29 +0300

    cors fix

--- src/main/java/akademia/demo/config/CorsConfig.java
+++ src/main/java/akademia/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
         .allowedOrigins("http://localhost:4200")
-        .allowedOrigins("http://207.180.212.53/:4200")
+        .allowedOrigins("http://207.180.212.53:4200")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
